e8eaeda8dffb53771f15e6e899c01e6f8370c0b9
morango9 <105944114+morango9@users.noreply.github.com>
2024-06-07 11:51:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,28 @@ const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-Resultados");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
             "Alternativa 1"
         "Alternativa 2"
         ]
@@ -21,3 +43,33 @@ const perguntas = [
     }
 ];
 
+        alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
