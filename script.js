7fec75cd81a5aea98180ac8d210c46879f069e12
morango9 <105944114+morango9@users.noreply.github.com>
2024-06-14 08:11:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultados = document.querySelector(".caixa-Resultados");
-const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-        ]
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-        ]
-    },
-    {
-        enunciado: "Pergunta 3",const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-Resultados");
