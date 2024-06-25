c047be2e5d33e2641f8d0db6578d362d2025396f
morango9 <105944114+morango9@users.noreply.github.com>
2024-06-25 15:05:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,67 +7,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "vc gosta de estuda?",
+        enunciado: "Estudar é um procedimento que exige disciplina, rigor, sentido de dever e responsabilidade. O aluno deve perceber significado no ato de estudar, reconhecer que o estudo irá lhe trazer um benefício, ou seja, estudar vale a pena. Você considera que estudar é importante?",
         alternativas: [
             {
-                texto: "isso é assustador",
-                afirmacao: "afirmacao",
+                texto: "sim, acho que estudar é importante!",
+                afirmacao: "Estudar ajuda a abrir a mente, a superar limitações e a desenvolver uma visão mais flexível. Além disso, ajuda a aprimorar habilidades, como capacidade de resolução de problemas, disciplina e responsabilidade.",
             },
             {
-                texto: "não",
-                afirmacao: "afirmacao",
+                texto: "Não, acho uma perda de tempo.",
+                afirmacao: "Sinto muito que pense assim, pois os estudos são importantes. Com eles adquirimos conhecimentos, cultura, e traçamos objetivos na vida.",
             },
         ],
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Atualmente somente 4.3% dos municípios do Paraná tem Sistema Próprio de Ensino,  95.7% de seus municípios estão vinculados ao sistema estadual de ensino, no qual se compreende que escolas públicas e particulares, devem seguir as diretrizes do Conselho Estadual de Educação.  Você está satisfeito com o método de ensino?",
         alternativas: [
             {
-                texto: "sim",
-                afirmacao: "afirmacao",
+                texto: "Sim, estou satisfeito com o ensino.",
+                afirmacao: "Que bom! O ensino oferece educação de qualidade, formando cidadãos com amplos conhecimentos humanísticos, científicos e tecnológicos , que potencializam suas competências pessoais e profissionais mediante os desafios do mundo do trabalho e do exercício efetivo da cidadania.",
             },
             {
-                texto: "não",
-                afirmacao: "afirmacao",
+                texto: "Não, acredito que poderia ser melhor.",
+                afirmacao: "O Sistema Estadual de Ensino existe para otimizar todas as atividades escolares e oferecer a melhor experiência ao aluno. Com uma estrutura pedagógica completa, que abrange inclusive o material didático.",
             },
         ],
     },
     {
-        enunciado: "Pergunta 3",
+        enunciado: "Estudos apontam que estudar pode aumentar a capacidade da memória, ativar o poder de concentração e, com o hábito, aumentar a nossa capacidade de aprender. Quanto mais estudamos e exercitamos o cérebro, mais protegido ele estará de doenças de degeneração, como o mal de Alzheimer, por exemplo. Você acredita que estudar faz bem para nossa saúde?",
         alternativas: [
             {
-                texto: "sim",
-                afirmacao: "afirmacao",
+                texto: "Sim, acredito!",
+                afirmacao: "A concentração é uma habilidade fundamental que pode ser aprimorada através do hábito de estudar todos os dias. Quando você se dedica a sessões regulares de estudo, treina sua mente para se concentrar em uma única tarefa por um período prolongado.",
             },
             {
-                texto: "não",
-                afirmacao: "afirmacao",
+                texto: "Não, acho que não tem nenhuma ligação.",
+                afirmacao: "Existem muitas pesquisas em andamento sobre a relação entre a recuperação de lesões cerebrais e o nível de escolaridade do indivíduo, mas o que já sabemos é que um dos benefícios de estudar é que a reserva cognitiva de quem passou anos e anos estudando é como um cofre. Nele, você vai guardando alguns conhecimentos que serão utilizados em casos de emergência.",
             },
         ],
     },
     {
-        enunciado: "Pergunta 4",
+        enunciado: "O cansaço constante proveniente de longas sessões de estudo pode levar à exaustão mental. Isso, por sua vez, pode prejudicar a capacidade de concentração e diminuir a motivação para estudar. Qual sua opinião sobre estudar muito?",
         alternativas: [
             {
-                texto: "sim",
-                afirmacao: "afirmacao",
+                texto: "Acho que estudar nunca é demais!",
+                afirmacao: "Estudar demais pode comprometer o seu aprendizado e até mesmo afetar a sua saúde. Ler, pesquisar e aprender obsessivamente pode fornecer uma falsa sensação de realização, sem que o conhecimento seja realmente incorporado e resulte em mais produtividade.",
             },
             {
-                texto: "não",
-                afirmacao: "afirmacao",
+                texto: "Acho que estudar muito ou pouco não muda.",
+                afirmacao: "É importante lembrar novamente que exagerar na quantidade de horas de estudo por dia pode causar exaustão e prejudicar seu desempenho durante a prova. Dessa forma, há uma média de quatro horas de estudo diário recomendada a fim de que você não se canse mentalmente.",
             },
         ],
     },
     {
-        enunciado: "Pergunta 5",
+        enunciado: "Sem uma rotina de estudos, pode ficar difícil reter uma grande quantidade de conteúdos. Então, uma das principais formas de superar a dificuldade de aprender é por meio de um planejamento diário. O que você faz quando está com dificuldade na aprendizagem?",
         alternativas: [
             {
-                texto: "sim",
-                afirmacao: "afirmacao",
+                texto: "Eu estudo até aprender a matéria",
+                afirmacao: "O cansaço é uma das causas mais alegadas por aqueles que reclamam que não conseguem estudar. Por isso, antes de desesperar-se, você já verificou se não precisa de um tempo para respirar? Anda dormindo bem? Comeu bem? Dormiu por pelo menos 6 horas? Estar descansado é essencial para uma boa absorção sobre os conteúdos.",
             },
             {
-                texto: "não",
-                afirmacao: "afirmacao",
+                texto: "Eu deixo para trás e vou para o próximo assunto.",
+                afirmacao: "Às vezes só precisamos de um tempo para assimilar, passar para um próximo assunto em que tenho mais afinidade talvez ajude. E depois, com mais tempo, mais descansado e mais paciencioso consiga aprender melhor o conteúdo.",
             },
         ],
     },
@@ -113,6 +113,24 @@ function mostraResultado(){
 
 
 mostraPergunta();
+
+
+
+
+      
+
+
+
+
+
+
+
+
+      
+
+
+
+
 
 
 
